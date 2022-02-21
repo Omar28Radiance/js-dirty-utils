d9d9615ff2679a7206201ee02dcbff469d1992b1
Omar Martínez <omarin0a@hotmail.com>
2022-02-21 16:31:56 -0600

    funcion multiplicacion

--- index.js
+++ index.js
@@ -6,15 +6,16 @@
 function suma(a,b){
     console.log (`La suma de ${a} mas ${b} es ${a+b}`)
 }
-
 suma(10,84)
 
 
 function resta(a,b){
     console.log (`La resta de ${a} menos ${b} es ${a-b}`)
 }
-
 resta(15,5)
+
+const multiplicacion=(a,b)=> console.log(`La multiplicacion de ${a} por ${b} es ${a*b}`)
+multiplicacion(10,5)
 /** 
  * @description console.log fn wrapper
  * @param {*} msn 
