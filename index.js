0415a6c3643e17da8c161b7ad9b288c380691928
Omar Martínez <omarin0a@hotmail.com>
2022-02-21 15:47:30 -0600

    funcion resta

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+
 function showNumber(number) {
-    console.log("El número ingresado" + number);
+    console.log(`El número ingresado" es ${number}`);
 };
+
+
+function resta(a,b){
+    console.log (`La resta de ${a} menos ${b} es ${a-b}`)
+}
+
+resta(15,5)
+/** 
+ * @description console.log fn wrapper
+ * @param {*} msn 
+*/
+// const logger = (msn) => console.log(msn);
+
+/** 
+ * @description sum two intg
+ * @param {Number} num1
+ * @param {Number} num2 
+*/
+// const add = (num1,num2) => logger(num1 + num2);
+
+// add(1,2);
